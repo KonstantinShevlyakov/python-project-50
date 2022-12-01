e7b6b904037130c8f4defac9cc1278fa2bbe049a
Konstantin Shevlyakov <k.shevlyakov@Konstantins-MacBook-Air.local>
2022-12-01 20:04:07 +0300

    fixtures added

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -2,9 +2,9 @@ import json
 from gendiff.scripts.gendiff import generate_diff
 
 
-file1 = 'test_files/file1.json'
-file2 = 'test_files/file2.json'
-result = open('test_files/result.txt', 'r').read()
+file1 = 'tests/fixtures/file1.json'
+file2 = 'tests/fixtures/file2.json'
+result = open('tests/fixtures/result.txt', 'r').read()
 
 
 def test_generate_diff():
