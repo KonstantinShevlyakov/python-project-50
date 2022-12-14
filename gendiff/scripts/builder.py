18736be5d5f763ab6922731696f1794a992260a9
Konstantin Shevlyakov <k.shevlyakov@Konstantins-MacBook-Air.local>
2022-12-15 00:22:14 +0300

    stylish added

--- gendiff/scripts/builder.py
+++ gendiff/scripts/builder.py
@@ -1,20 +1,34 @@
 #!usr/bin/env python3
 
+
+def primitive_stringify(value):
+    if isinstance(value, dict):
+        return value
+    elif value is True:
+        return 'true'
+    elif value is False:
+        return 'false'
+    elif value is None:
+        return 'null'
+    else:
+        return str(value)
+
+
 def build_representation(d1, d2):
     result = {}
     unique_keys = d1.keys() | d2.keys()
     sorted_keys = sorted(unique_keys, key=lambda x: x)
     for k in sorted_keys:
         if k in d1.keys() and k not in d2.keys():
-            result[k] = ('unchanged', d1[k])
+            result[k] = ('unchanged', primitive_stringify(d1[k]))
         if k not in d1.keys() and k in d2.keys():
-            result[k] = ('added', d2[k])
+            result[k] = ('added', primitive_stringify(d2[k]))
         if k in d1.keys() and k in d2.keys():
             if d1[k] != d2[k]:
                 if isinstance(d1[k], dict) and isinstance(d2[k], dict):
                     result[k] = ('changed', build_representation(d1[k], d2[k]))
                 else:
-                    result[k] = ('two_values', d1[k], d2[k])
+                    result[k] = ('two_values', primitive_stringify(d1[k]), primitive_stringify(d2[k]))
             else:
-                result[k] = ('saved', d1[k])
+                result[k] = ('saved', primitive_stringify(d1[k]))
     return result
--- gendiff/scripts/stylish.py
+++ gendiff/scripts/stylish.py
@@ -2,26 +2,67 @@
 from itertools import chain
 
 
+def stringify_dict(value, depth, space_count=2):
+    if not isinstance(value, dict):
+        return str(value)
+
+    deep_size = depth + space_count
+    deep_tab = deep_size * ' '
+    current_tab = depth * ' '
+    lines = []
+    for key, val in value.items():
+        lines.append(f'{deep_tab}   {key}: {stringify_dict(val, deep_size + 2)}')
+    result = chain("{", lines, [deep_tab + "}"])
+    return '\n'.join(result)
+
+
 def stylish(data, space_count=2):
     def iter_(data, iter):
+
         lines = []
         deep_size = iter + space_count
-        deep_tab = deep_size * ' '
+        # deep_tab = deep_size * ' '
         current_tab = iter * ' '
         for k in data:
             if data[k][0] == 'added':
-                lines.append(f'{current_tab} + {k}: {data[k][1]}')
+                if isinstance(data[k][1], dict):
+                    lines.append(
+                        f'{current_tab} + {k}: {stringify_dict(data[k][1], deep_size)}'
+                    )
+                else:
+                    lines.append(f'{current_tab} + {k}: {data[k][1]}')
             elif data[k][0] == 'unchanged':
-                lines.append(f'{current_tab} - {k}: {data[k][1]}')
+                if isinstance(data[k][1], dict):
+                    lines.append(
+                        f'{current_tab} - {k}: {stringify_dict(data[k][1], deep_size)}'
+                    )
+                else:
+                    lines.append(f'{current_tab} - {k}: {data[k][1]}')
             elif data[k][0] == 'saved':
                 lines.append(f'{current_tab}   {k}: {data[k][1]}')
             elif data[k][0] == 'two_values':
-                lines.append(f'{current_tab} - {k}: {data[k][1]}')
-                lines.append(f'{current_tab} + {k}: {data[k][2]}')
+                if isinstance(data[k][1], dict) and not isinstance(data[k][2], dict):
+                    lines.append(
+                        f'{current_tab} - {k}: {stringify_dict(data[k][1], deep_size)}')
+                    lines.append(f'{current_tab} + {k}: {data[k][2]}')
+                elif not isinstance(data[k][1], dict) and isinstance(data[k][2], dict):
+                    lines.append(f'{current_tab} - {k}: {data[k][1]}')
+                    lines.append(
+                        f'{current_tab} + {k}: {stringify_dict(data[k][1], deep_size)}'
+                    )
+                elif isinstance(data[k][1], dict) and isinstance(data[k][2], dict):
+                    lines.append(
+                        f'{current_tab} - {k}: {stringify_dict(data[k][1], deep_size)}')
+                    lines.append(
+                        f'{current_tab} + {k}: {stringify_dict(data[k][1], deep_size)}'
+                    )
+                else:
+                    lines.append(f'{current_tab} - {k}: {data[k][1]}')
+                    lines.append(f'{current_tab} + {k}: {data[k][2]}')
             elif data[k][0] == 'changed':
-                lines.append(f'{deep_tab} {k}: {iter_(data[k][1], deep_size)}')
+                lines.append(f'{current_tab}   {k}: {iter_(data[k][1], deep_size + 2)}')
 
-        result = chain("{", lines, [current_tab + "}"])
+            result = chain("{", lines, [current_tab + "}"])
         return '\n'.join(result)
 
     return iter_(data, 0)
